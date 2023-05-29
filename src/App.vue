012a52e6688618d817c034ae234cd1ff9b2c8120
StefanoGalandrini <stefe68@gmail.com>
2023-05-29 17:25:04 +0200

    Init repo  (previously deleted) with base files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+	components: {
+		AppHeader,
+	},
+};
+</script>
+
+<template>
+	<AppHeader />
+</template>
+
+<style lang="scss">
+@use "./assets/styles/general.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<script>
+export default {};
+</script>
+
+<template>
+	<header>
+		<nav>
+			<img
+				src="../assets/images/avada-bakery-logo-retina-200x97.png"
+				alt="Logo" />
+			<ul class="menu">
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Shop</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+nav {
+	height: 10vh;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	ul {
+		height: 100%%;
+		list-style-type: none;
+		display: flex;
+		gap: 1.5rem;
+
+		a {
+			text-decoration: none;
+		}
+	}
+}
+</style>
